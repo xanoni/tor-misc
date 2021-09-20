affb66aaeba30a0957acccb1e5a9898393293843
xanoni <77220130+xanoni@users.noreply.github.com>
2021-09-20 01:30:35 -0400

    Add: torrc-geonode-generator.sh

---
+++ torrc-geonode-generator.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env -S bash -e
+
+infile="${1}"
+
+if [[ "${infile}" == '' ]]; then
+    echo -en "\nPlease provide input file!\n\n" && exit 1
+fi
+
+countries="$(awk '{ ORS=","; print $1 }' "${infile}" | sed 's/,$//')"
+outfile="$(dirname "${infile}")/${infile%%.*}-rcline.${infile##*.}"
+
+echo -en "# exclude known surveillance countries that we may not trust" \
+         "\nStrictNodes   1" \
+         "\nExcludeNodes  {US},{CA},{AU},{NZ},{GB}\n" \
+         "\n# only use one of (EntryNodes|MiddleNodes|ExitNodes) at a time!" \
+         "\nEntryNodes    ${countries}" \
+         "\nMiddleNodes   ${countries}" \
+         "\nExitNodes     ${countries}\n" \
+         > "${outfile}"
+
+echo -en "\nOutput written to: ${outfile}\n\n"
