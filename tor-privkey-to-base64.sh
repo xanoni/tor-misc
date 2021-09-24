3ed9677b621fcd4620858ba1645f09b1de2a30c9
xanoni <77220130+xanoni@users.noreply.github.com>
2021-09-24 01:33:27 -0400

    Clean-up: tor-privkey-to-base64.sh

--- tor-privkey-to-base64.sh
+++ tor-privkey-to-base64.sh
@@ -1,22 +1,18 @@
 #! /usr/bin/env -S bash -ex
+# vim: set ts=4 sts=4 et sw=4 ft=bash :
+
+OUTFILE="onion_v3_private_key.new"
 
 if [ -z "${1}" ]; then
-    echo "Please pass privkey to be base64 encoded."
-    exit 1
+    echo "Please pass privkey to be base64 encoded." && exit 1
 fi
 
-echo -en 'ED25519-V3:' > onion_v3_private_key.new
+echo -n 'ED25519-V3:' > "${OUTFILE}"
 
-if [ "${OSTYPE}" = "linux-gnu" ] || [ "${OSTYPE}" = "linux-gnueabihf" ]; then
-    echo "Linux"
-    dd if="${1}" bs=1 skip=32 | base64 -w0 >> onion_v3_private_key.new
-elif [ "${OSTYPE}" = "darwin20" ]; then
-    echo "MacOS"
-    dd if="${1}" bs=1 skip=32 | base64 >> onion_v3_private_key.new
+if [[ "${OSTYPE}" != darwin20* ]]; then
+    dd if="${1}" bs=1 skip=32 | base64 -w0 >> "${OUTFILE}"
 else
-    echo "Error!"
-    rm onion_v3_private_key.new
-    exit 1
+    dd if="${1}" bs=1 skip=32 | base64 >> "${OUTFILE}"
 fi
 
 echo "Done!"
